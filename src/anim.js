4858a1b8120ae027d2bdfa25f0b8426e6de14c4a
caesarsol <smilemachine@gmail.com>
2017-04-02 19:57:19 +0200

    💥 Initial commit, working prototype

---
+++ src/anim.js
@@ -0,0 +1,64 @@
+import d3 from 'd3'
+
+function animAttrs(oldVnode, vnode) {
+  const elm = vnode.elm
+  let oldAttrs = oldVnode.data.anim
+  let attrs = vnode.data.anim
+  const params = vnode.data.animParams
+
+  if (!oldAttrs && !attrs) return
+  if (oldAttrs === attrs) return
+  oldAttrs = oldAttrs || {}
+  attrs = attrs || {}
+
+  const attrsToUpdate = []
+
+  // update modified attributes, add new attributes
+  for (let key in attrs) {
+    const cur = attrs[key]
+    const old = oldAttrs[key]
+    if (old === cur) continue
+    attrsToUpdate.push({ key, value: cur })
+  }
+
+  // remove removed attributes
+  for (let key in oldAttrs) {
+    if (key in attrs) continue
+    const zero = 0 // Should check for values other than numbers, such as colors
+    attrsToUpdate.push({ key, value: zero, then: () => { elm.removeAttribute(key) } })
+  }
+
+  if (attrsToUpdate.length === 0) return
+
+  const transition = d3.select(elm)
+    .transition()
+    .duration(params.duration)
+    .ease(params.ease)
+
+  attrsToUpdate.forEach(({ key, value, then }) => {
+    transition.attr(key, value)
+    if (then) transition.each('end', then)
+  })
+}
+
+function removeAttrsAnim(vnode, done) {
+  const elm = vnode.elm
+  const attrs = vnode.data.anim
+  if (!attrs) return
+  const params = vnode.data.animParams
+  const transition = d3.select(elm)
+    .transition()
+    .duration(params.duration)
+    .ease(params.ease)
+
+  for (let key in attrs) {
+    const attr = attrs[key]
+    if (!Number.isFinite(attr)) return
+    const zero = 0 // Should check for values other than numbers, such as colors
+    transition.attr(key, zero)
+  }
+
+  transition.each('end', done)
+}
+
+export default { create: animAttrs, update: animAttrs, remove: removeAttrsAnim }
---
+++ src/index.js
@@ -0,0 +1,54 @@
+import snabbdom, { h } from 'snabbdom'
+import anim from './anim'
+
+const patch = snabbdom.init([
+  require('snabbdom/modules/class').default, // makes it easy to toggle classes
+  require('snabbdom/modules/attributes').default, // for setting attributes on DOM elements (domEl.setAttribute(asd))
+  require('snabbdom/modules/props').default, // for setting properties on DOM elements (domEl[prop] = asd)
+  require('snabbdom/modules/style').default, // handles styling on elements with support for animations
+  require('snabbdom/modules/eventlisteners').default, // attaches event listeners
+  anim,
+])
+
+const container = document.getElementById('root')
+
+function osc(x) { return (1 + Math.sin(x)) / 2 }
+
+function genData(key) {
+  return Array(50).fill(0).map((_,i) => ({ x: i, y: 5 + osc(i/2.5 + key) * osc(i/4 + key) * 100 }))
+}
+
+function view(data) {
+  return (
+    h('div#container', { on: { click: rerender } }, [
+      h('svg', { attrs: { width: 1300, height: 100 } },
+        data.map(({x, y}) =>
+          y < 10 ? null :
+          h('rect', {
+            key: x,
+            attrs: { x: x * 15, width: 10, height: 0, fill: 'tomato', stroke: 'none', strokeWidth: 1 },
+            anim: { height: y, fill: y > 90 ? 'blue' : 'tomato' },
+            animParams: { duration: 500, ease: 'cubic-in-out' },
+          })
+        ).filter(Boolean)
+      )
+    ])
+  )
+}
+
+let vnode = container
+function render(data) {
+  // Patch into empty DOM element – this modifies the DOM as a side effect
+  vnode = patch(vnode, view(data))
+}
+
+function rerender() {
+  render(genData(Date.now() / 1000))
+}
+
+rerender()
+
+if (1)
+  window.setInterval(() => {
+    rerender()
+  }, 500)
